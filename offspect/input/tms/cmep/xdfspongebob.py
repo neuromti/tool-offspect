22a25cb007e2a499278890f83456a53fc7aacaf4
Julian-Samuel Gebühr <Julian-Samuel Gebhr>
2020-04-14 17:09:49 +0200

    Refactor spongebob loader

--- offspect/input/tms/cmep/xdfspongebob.py
+++ offspect/input/tms/cmep/xdfspongebob.py
@@ -9,14 +9,7 @@ This kind of file format is our preferred file format. It is `open-source, well-
 Data
 ****
 
-Because LabRecorder can record multiple streams into a single :code:`.xdf`-file. These files can contain therefore not only EEG and EMG, but also e.g. pupilometric data, respiration effort, grip force, and many more. As it allows to record multiple streams, it also offers the option to record coordinates (as e.g. sent with every pulse from localite version 4.0) together with the raw data (as sent e.g. by eego or bvr) and additional markers. 
-
-Coordinates
-***********
-
-In the optimal case, the :code:`.xdf`-file contains already sufficient information about the coordinates, and pairing is automatic. Yet, there will be some :code:`.xdf`-files, where not all streams were recorded. This might have happened e.g. due to errors in the recording script, an erroneous automated recording, or during manual recording with LabRecorder. In these cases, information about coordinates or other markers can be missing. The pairing of coordinates with a specific trace needs to be reconstructed manually (see :ref:`support-link-coords`).
- 
-If multiple protocols were recorded in one :code:`xdf`-file, as often happened during manual recording, we will have hundreds of stimuli. Worse, it can be that even marker-streams are missing, and there is no information when a protocol started within the long recording. Linking them to the correct coordinates is tricky, and the best chance is probably taking account of the relative latency between subsequent stimuli.
+The data is espected to have TMS pulses, triggered by spongebob.
 
 """
 from offspect.types import Annotations, FileName
@@ -52,29 +45,23 @@ def prepare_annotations(
     channel: str,
     pre_in_ms: float,
     post_in_ms: float,
-    xmlfile: FileName = None,
-    event_name="coil_0_didt",
-    event_mark="coil_0_didt",
-    event_stream="localite_marker",
+    event_name="Spongebob-Trigger",
+    event_mark=1,
+    event_stream="Spongebob-Data",
     comment_name=None,
 ) -> Annotations:
-    """load a documentation.txt and cnt-files and distill annotations from them
-    
+    """ 
     args
     ----
-    xmlfile: FileName
-        an option xml file with information about the target coordinates 
 
-    readout: str
-        which readout to use
+    xdffile: FileName
+        the :code:`.xdf`-file with the recorded streams, e.g. data and markers
     channel: str
         which channel to pick
     pre_in_ms: float
         how many ms to cut before the tms
     post_in_ms: float
         how many ms to cut after the tms
-    xdffile: FileName
-        the :code:`.xdf`-file with the recorded streams, e.g. data and markers
     returns
     -------
     annotation: Annotations
@@ -88,15 +75,6 @@ def prepare_annotations(
     time_stamps = [ts for ts in yield_timestamps(event_stream, event_mark)]
     event_count = len(time_stamps)
 
-    if (("localite_flow" in streams or "localite_marker") in streams) and not("Spongebob-Data" in streams):
-        loc_stream = streams["localite_marker"]
-        coords = list(yield_loc_coords(loc_stream, time_stamps))
-        stimulation_intensity_didt = list(yield_loc_didt(loc_stream, time_stamps))
-        stimulation_intensity_mso = list(yield_loc_mso(loc_stream, time_stamps))
-    else:
-        coords = list_nan_coords(event_count)
-        stimulation_intensity_didt = list_nan(event_count)
-        stimulation_intensity_mso = list_nan(event_count)
     print(f"Found {event_count} events")
 
     if "reiz_marker_sa" in streams and comment_name is not None:
@@ -138,10 +116,7 @@ def prepare_annotations(
             "event_name": event_stream.name + "-" + str(event_name),
             "event_sample": event_samples[idx],
             "event_time": event_times[idx],
-            "xyz_coords": coords[idx],
             "time_since_last_pulse_in_s": time_since_last_pulse[idx],
-            "stimulation_intensity_mso": stimulation_intensity_mso[idx],
-            "stimulation_intensity_didt": stimulation_intensity_didt[idx],
         }
         anno.append_trace_attr(tattr)
     return anno.anno
@@ -175,18 +150,4 @@ def cut_traces(xdffile: FileName, annotation: Annotations) -> List[TraceData]:
         trace = datastream.time_series[onset - pre : onset + post, cix]
         traces.append(trace)
     return traces
-#%%
 
-if __name__ == "__main__":
-    filename = "/home/juli/acute_study/recordings/NoDi/IMONAS-acute-study-intervention_R001.xdf"
-    anno = prepare_annotations(
-            xdffile = filename,
-            channel = "EDC_R",
-            pre_in_ms = 100,
-            post_in_ms = 100,
-            event_stream="Spongebob-Data",
-            event_mark = 1,
-            event_name = "Spongebob-Trigger",
-            comment_name="Stimulus comment"
-            )
-    traces = cut_traces(xdffile, anno)
