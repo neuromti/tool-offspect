058d885853c034119e072c92338ef974aae0d2ef
agricolab <robertsadresse@gmx.de>
2020-06-05 10:28:20 +0200

    Refactor cleaning tattrs inot own method
    Resolve #38

--- offspect/gui/VWidgets/tattrs.py
+++ offspect/gui/VWidgets/tattrs.py
@@ -19,26 +19,11 @@ class TattrWidget(QtWidgets.QWidget):
 
     def __init__(self, cf: CacheFile, idx: int, *args, **kwargs):
         super(TattrWidget, self).__init__(*args, **kwargs)
-        tattrs = cf.get_trace_attrs(idx)
 
-        set_init = [
-            "onset_shift",
-            "neg_peak_latency_ms",
-            "pos_peak_latency_ms",
-            "neg_peak_magnitude_uv",
-            "pos_peak_magnitude_uv",
-        ]
-        for key in set_init:
-            tattrs[key] = encode(decode(tattrs[key]) or 0)
-        cf.set_trace_attrs(idx, tattrs)
-
-        keys = get_valid_trace_keys(tattrs["readin"], tattrs["readout"]).copy()
-        keys.remove("reject")
-        keys.remove("onset_shift")
-        keys.remove("comment")
+        tattrs = self.get_cleaned_tattrs(cf, idx)
         layout = QtWidgets.QGridLayout()
         row = 0
-        for key in sorted(keys):
+        for key in tattrs.keys():
             label = QtWidgets.QLabel(text=key.replace("_", " ").capitalize())
             if type(decode(tattrs[key])) == float:
                 val = "{0:3.3f}".format(decode(tattrs[key]))
@@ -53,6 +38,7 @@ class TattrWidget(QtWidgets.QWidget):
             row += 1
 
         key = "comment"
+        tattrs = cf.get_trace_attrs(idx)
         label = QtWidgets.QLabel(text=key)
         line = VTextEdit(tattrs[key])
         trig = partial(save, cf=cf, idx=idx, key=key, read=line.toPlainText)
@@ -61,3 +47,24 @@ class TattrWidget(QtWidgets.QWidget):
         layout.addWidget(line, row, 1)
 
         self.setLayout(layout)
+
+    def get_cleaned_tattrs(self, cf, idx):
+        tattrs = cf.get_trace_attrs(idx)
+        set_init = [
+            "onset_shift",
+            "neg_peak_latency_ms",
+            "pos_peak_latency_ms",
+            "neg_peak_magnitude_uv",
+            "pos_peak_magnitude_uv",
+        ]
+        for key in set_init:
+            tattrs[key] = encode(decode(tattrs[key]) or 0)
+        cf.set_trace_attrs(idx, tattrs)
+        keys = get_valid_trace_keys(tattrs["readin"], tattrs["readout"]).copy()
+        keys.remove("reject")
+        keys.remove("onset_shift")
+        keys.remove("comment")
+        show_tattrs = dict()
+        for key in sorted(keys):
+            show_tattrs[key] = tattrs[key]
+        return show_tattrs
